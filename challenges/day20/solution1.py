8789b486632079dc844a8543cbc3590fb8caacf2
Sven Mayer <svn.myr@gmail.com>
2024-09-24 21:36:59 +0200

    Added solution 2.

--- challenges/day20/solution1.py
+++ challenges/day20/solution1.py
@@ -1,4 +1,13 @@
 #! /usr/bin/python3
+PLOT_DATA = False
+
+if PLOT_DATA:
+    import matplotlib.pyplot as plt
+    plt.close("all")
+
+import numpy as np
+
+
 
 TEST_DATA1 = """broadcaster -> a, b, c
 %a -> b
@@ -18,7 +27,7 @@ class Node:
         self.name = name
         self.no_inputs = no_inp
         self.state = 0
-    
+
     def set_input(self, inp, pulse):
         pass
 
@@ -26,7 +35,7 @@ class Node:
 class Broadcast(Node):
     def __init__(self, *args, **kwargs):
         Node.__init__(self, *args, **kwargs)
-    
+
     def set_input(self, inp, pulse):
         return pulse
 
@@ -34,17 +43,17 @@ class Broadcast(Node):
 class FlipFlop(Node):
     def __init__(self, *args, **kwargs):
         Node.__init__(self, *args, **kwargs)
-    
+
     def set_input(self, inp, pulse):
         if pulse == 0:
             self.state = (self.state + 1) % 2
             return self.state
-    
+
 class Conjecture(Node):
     def __init__(self, *args, **kwargs):
         Node.__init__(self, *args, **kwargs)
         self.allset_comp = 2**self.no_inputs - 1
-    
+
     def set_input(self, inp, pulse):
         if pulse:
             self.state |= 1 << inp
@@ -57,7 +66,7 @@ class Conjecture(Node):
 class Blank(Node):
     def __init__(self, *args, **kwargs):
         Node.__init__(self, *args, **kwargs)
-    
+
     def set_input(self, inp, pulse):
         return None
 
@@ -66,8 +75,10 @@ class Network:
     def __init__(self, data):
         self.network, self.broadcast = self.parse_input(data)
         self.rx_pressed = False
+        self.button_count = 0
+        self.mg_states = []
 
-    @staticmethod     
+    @staticmethod
     def parse_input(data):
         no_inputs = dict()
         idx_dict = dict()
@@ -116,6 +127,7 @@ class Network:
         return network, broadcast
 
     def push_button(self):
+        self.button_count += 1
         lowhigh = [0, 0]
         events = [(self.broadcast, 0, 0)]
         while len(events):
@@ -125,6 +137,8 @@ class Network:
             if n.name == "rx" and pulse == 0:
                 self.rx_pressed = True
             res = n.set_input(inp, pulse)
+            if n.name == "mg":
+                self.mg_states.append((self.button_count, n.state))
             if res is None:
                 continue
             events.extend([(o[0], o[1], res) for o in outs])
@@ -150,7 +164,7 @@ class Network:
             high_pulse.append(high_pulse[-1] + lh[1])
             n += 1
         return low_pulse, high_pulse
-    
+
     def get_pulses(self, nbutton):
         low_list, high_list = self.cycle_network(nbutton)
         n_cylce = len(low_list)
@@ -167,7 +181,49 @@ def solution1(data):
     return lw[0] * lw[1]
 
 
-print(solution1(TEST_DATA2))
+def solution2(data):
+    network = Network(data)
+    for i in range(10000):
+        network.push_button()
+    I = [], [], [], [], []
+    B = set(), set(), set(), set()
+    for ln in network.mg_states:
+        i0 = ln[1] & 0b1
+        i1 = (ln[1] & 0b10) // 0b10
+        i2 = (ln[1] & 0b100) // 0b100
+        i3 = (ln[1] & 0b1000) // 0b1000
+        I[0].append(ln[0])
+        I[1].append(i0)
+        I[2].append(i1)
+        I[3].append(i2)
+        I[4].append(i3)
+        if i0:
+            B[0].add(ln[0])
+        if i1:
+            B[1].add(ln[0])
+        if i2:
+            B[2].add(ln[0])
+        if i3:
+            B[3].add(ln[0])
+    if PLOT_DATA:
+        fig, ax = plt.subplots(5, 1, sharex=True)
+        for i, ax_ in enumerate(ax):
+            ax_.plot(I[i])
+        fig.show()
+
+    B_CTR = [sorted([itm for itm in B[i]]) for i in range(len(B))]
+    lcm_1 = np.lcm(B_CTR[0][0], B_CTR[1][0])
+    lcm_2 = np.lcm(B_CTR[2][0], B_CTR[3][0])
+    return np.lcm(lcm_1, lcm_2)
+
 
 with open("../../inputs/day20/input", "r") as fid:
-    print(solution1(fid.read()))
+    print("Test Data")
+    print(solution1(TEST_DATA2))
+
+    data = fid.read()
+    print("Solution 1")
+    print(solution1(data))
+
+    print("Solution 2")
+    print(solution2(data))
